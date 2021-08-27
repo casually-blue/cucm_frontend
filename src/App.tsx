298738597ceb4335a7e81bb98e6ca802d0d72fc0
Thomas Terry <darkforestsilence@gmail.com>
2021-08-26 21:35:33 -0500

    Add cucm

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import {HashRouter, Route, NavLink} from "react-router-dom";
 import {Office} from "./Pages/Office";
 import {Home} from "./Pages/Home";
 import {Fidelis} from "./Pages/Fidelis";
+import {CUCM} from "./Pages/CUCM";
 
 export default class App extends Component {
     render(): JSX.Element {
@@ -16,12 +17,14 @@ export default class App extends Component {
                             <li><NavLink exact to="/">Home</NavLink></li>
                             <li><NavLink exact to="/office365">Office 365</NavLink></li>
                             <li><NavLink exact to="/fidelis">Fidelis</NavLink></li>
+                            <li><NavLink exact to="/cucm">CUCM</NavLink></li>
                         </ul>
                     </div>
                     <div className="row content">
                         <Route exact path="/" component={Home}/>
                         <Route exact path="/office365" component={Office}/>
                         <Route exact path="/fidelis" component={Fidelis}/>
+                        <Route exact path="/cucm" component={CUCM}/>
                     </div>
                     <div className="row foot">
 
---
+++ src/Pages/CUCM.tsx
@@ -0,0 +1,12 @@
+import {Component} from "react";
+
+export class CUCM extends Component {
+    render(): JSX.Element {
+        return (
+            <div>
+                <h2>CUCM Page</h2>
+                <p>Sample Text</p>
+            </div>
+        );
+    }
+}
